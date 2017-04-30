ba80fcbda21d90577807970ba18d2c5048774869
Aaron Connolly <aaronconnolly@yahoo.com>
2017-04-30 22:20:57 +0100

    First commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    @IBAction func buttonTapped(_ sender: Any) {
+        // if button tapped print hello world
+        tapCount = tapCount + 1
+        print (tapCount)
+        if tapCount > 9 {
+            theLabel.text = "Woah woah woah!"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
