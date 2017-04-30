50f5dae22f6ee81f276b55b4fac4a461e90428af
Aaron Connolly <aaronconnolly@yahoo.com>
2017-05-01 00:24:24 +0100

    update

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -11,11 +11,22 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var theLabel: UILabel!
     
+    @IBOutlet weak var text1: UITextField!
+    
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    
+    
+    
     var tapCount = 0
     @IBAction func buttonTapped(_ sender: Any) {
         // if button tapped count
         tapCount = tapCount + 1
         print (tapCount)
+        print (text1.text!)
+        print (text2.text!)
         if tapCount > 9 {
             theLabel.text = "Woah woah woah!"
         }
@@ -23,6 +34,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
         
         // Do any additional setup after loading the view, typically from a nib.
     }
