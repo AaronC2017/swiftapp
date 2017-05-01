13622bf9689955074053f5897082f376f701bc54
Aaron Connolly <aaronconnolly@yahoo.com>
2017-05-01 01:12:42 +0100

    boolean

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -21,25 +21,34 @@ class ViewController: UIViewController {
     
     
     var tapCount = 0
+    var addition = false
+    
     @IBAction func buttonTapped(_ sender: Any) {
         // if button tapped count
         tapCount = tapCount + 1
-        theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        if addition {
+            theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+            
+        } else {
+            theLabel.text = "\(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
         
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.red
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
